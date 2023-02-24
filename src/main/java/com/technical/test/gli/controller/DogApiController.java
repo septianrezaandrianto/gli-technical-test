3153985cd018e09f910d8610b26fc8d87d866716
Septian Reza Andrianto <septianreza.bahana@gmail.com>
2023-02-24 17:27:00 +0700

    add controller for get all list

--- src/main/java/com/technical/test/gli/controller/DogApiController.java
+++ src/main/java/com/technical/test/gli/controller/DogApiController.java
@@ -1,5 +1,6 @@
 package com.technical.test.gli.controller;
 
+import com.technical.test.gli.dto.DogApiResponse;
 import com.technical.test.gli.dto.DogResponse;
 import com.technical.test.gli.dto.DogSubBreedRequest;
 import com.technical.test.gli.service.DogApiService;
@@ -16,6 +17,10 @@ public class DogApiController {
     @Autowired
     private DogApiService dogApiService;
 
+    @GetMapping("/getDogDataList")
+    public ResponseEntity<DogApiResponse> getDogDataList() {
+        return ResponseEntity.ok(dogApiService.getDogDataList());
+    }
     @PostMapping("/createDogData")
     public ResponseEntity<DogResponse> createDogData(@RequestBody DogSubBreedRequest request) {
         return ResponseEntity.ok(dogApiService.createDogData(request));
